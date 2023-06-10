25d9d3b235fc3797ac310b600f4201312c996497
Nikolai <nikolaitodorov119@gmail.com>
2023-06-10 17:43:58 +0300

    1 - created HashMap to store info 2 - using MicrosoftOffice Excel as database 3 - Options for getting all products info or specific product info 4 - Added new POI dependencies

--- src/main/java/io/github/ndimovt/FileReading.java
+++ src/main/java/io/github/ndimovt/FileReading.java
@@ -1,38 +1,60 @@
 package io.github.ndimovt;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileReading {
-    private ArrayList<String> readInformation() {
-        ArrayList<String> agricultureProducts = new ArrayList<>();
+    private Map<String, String> readInformation(String year) {
+        Map<String, String> agricultureInfo = new HashMap<>();
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\Nikolai\\IdeaProjects\\PriceCalculator\\Agricultire.txt"));
-            String line;
-            while((line = bufferedReader.readLine()) != null){
-                agricultureProducts.add(line);
+            FileInputStream fileReader = new FileInputStream("C:\\Users\\Nikolai\\Desktop\\Prices.xlsx");
+            Workbook readInfo = new XSSFWorkbook(fileReader);
+            Sheet sheet = readInfo.getSheet(year);
+            for(Row row: sheet){
+                Cell keyCell = row.getCell(0);
+                Cell valueCell = row.getCell(1);
+                if((keyCell != null) && (valueCell != null)){
+                    String key = keyCell.toString();
+                    String value = valueCell.toString();
+                    agricultureInfo.put(key,value);
+                }
             }
-            closingStreams(bufferedReader);
+            closingStreams(readInfo,fileReader);
         } catch (FileNotFoundException fnf) {
             System.out.println("Missing file!");
             fnf.printStackTrace();
         }catch (IOException ie){
             ie.printStackTrace();
         }
-        return agricultureProducts;
+        return agricultureInfo;
     }
-    public void showInfo(){
-        ArrayList<String> information = readInformation();
-        for(String info : information){
-            System.out.println(info);
+    public void getSpecificAgricultureInfo(String year, String agricultureProduct){
+        for(Map.Entry<String,String> specificProductInfo : readInformation(year).entrySet()){
+            if(specificProductInfo.getKey().contains(agricultureProduct)){
+                System.out.println(specificProductInfo.getKey()+" : "+specificProductInfo.getValue());
+            }else {
+                System.out.println("Database doesn't contain information for "+agricultureProduct);
+            }
         }
     }
-    private void closingStreams(BufferedReader bfr) throws IOException{
-        if(bfr.readLine() != null){
-            bfr.close();
+    public void getEntireAgricultureProductInfo(String year){
+        for(Map.Entry<String, String> allProductsInfo : readInformation(year).entrySet()){
+            System.out.println(allProductsInfo.getKey()+" : "+allProductsInfo.getValue());
+        }
+    }
+    private void closingStreams( Workbook workbook, FileInputStream fileInputStream) throws IOException{
+        if(workbook != null){
+            workbook.close();
+        }
+        if(fileInputStream != null){
+            fileInputStream.close();
         }
     }
 }
--- src/main/java/io/github/ndimovt/Main.java
+++ src/main/java/io/github/ndimovt/Main.java
@@ -1,8 +1,19 @@
 package io.github.ndimovt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        new FileReading().showInfo();
-
+        try {
+            Scanner inn = new Scanner(System.in);
+            System.out.println("Enter year to get prices");
+            String year = inn.nextLine();
+            System.out.println("Enter agriculture name");
+            String agriculture = inn.nextLine();
+            new FileReading().getSpecificAgricultureInfo(year, agriculture);
+        } catch (NullPointerException npe) {
+            System.out.println("No information for that year");
+            npe.printStackTrace();
+        }
     }
 }
