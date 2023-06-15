895be1d00af4f4c1965dcdf232434b685b464f9a
Nikolai <nikolaitodorov119@gmail.com>
2023-06-15 21:43:02 +0300

    1- Created seperate class for getting info from Database 2 - Created seperate class for reading info from Database

--- src/main/java/io/github/ndimovt/Main.java
+++ src/main/java/io/github/ndimovt/Main.java
@@ -8,9 +8,10 @@ public class Main {
             Scanner inn = new Scanner(System.in);
             System.out.println("Enter year to get prices");
             String year = inn.nextLine();
-            System.out.println("Enter agriculture name");
-            String agriculture = inn.nextLine();
-            new DBReading().getSpecificAgricultureInfo(year, agriculture);
+            //System.out.println("Enter agriculture name");
+            //String agriculture = inn.nextLine();
+            //new DBReading().getSpecificAgricultureInfo(year, agriculture);
+            new DBReading().getEntireAgricultureProductInfo(year);
         } catch (NullPointerException npe) {
             System.out.println("No information for that year");
             npe.printStackTrace();
