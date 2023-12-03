d2ae9b5bf3ebc0615468e30fd1a09ab92e0aefda
ndimovt <nikolaitodorov119@gmail.com>
2023-12-03 15:15:07 +0200

    removed unused dependencies added <developers> in pom.xml

--- src/main/java/io/github/ndimovt/CalculateProfit.java
+++ src/main/java/io/github/ndimovt/CalculateProfit.java
@@ -2,14 +2,15 @@ package io.github.ndimovt;
 
 import java.text.DecimalFormat;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class CalculateProfit {
     private DecimalFormat df = new DecimalFormat("0.00");
-    public double calculationOfProfit(int year, String agriProductName, double productQuantity) throws NullPointerException{
+    public double calculationOfProfit(int year, String agriProductName, double productQuantity) throws NoSuchElementException {
         CreateHashMap createHashMap = new CreateHashMap();
         Map<String, Double> af = createHashMap.populateMap(year);
         double value = af.get(agriProductName);
-        String profit = df.format(value*productQuantity);
+        String profit = df.format(value * productQuantity);
         return Double.parseDouble(profit);
+        }
     }
-}
